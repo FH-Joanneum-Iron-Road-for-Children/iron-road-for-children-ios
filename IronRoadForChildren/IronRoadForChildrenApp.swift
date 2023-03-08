a05405ebd9a232356c295daf15add9ab0aa3edac
Alexander Kauer <contact@kaulex.dev>
2023-03-08 17:03:56 +0100

    #1 added Xcode project

---
+++ IronRoadForChildren/IronRoadForChildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IronRoadForChildrenApp.swift
+//  IronRoadForChildren
+//
+//  Created by Alexander Kauer on 08.03.23.
+//
+
+import SwiftUI
+
+@main
+struct IronRoadForChildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

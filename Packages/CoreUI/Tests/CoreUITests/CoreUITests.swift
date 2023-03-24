2ae4ce7d6728999e94e2caa3efefa5a82e27a62d
Alexander Kauer <contact@kaulex.dev>
2023-03-24 22:30:47 +0100

    #37 add icons, colors and core components

--- Packages/CoreUI/Tests/CoreUITests/CoreUITests.swift
+++ Packages/CoreUI/Tests/CoreUITests/CoreUITests.swift
@@ -6,6 +6,6 @@ final class CoreUITests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CoreUI().text, "Hello, World!")
+//        XCTAssertEqual(CoreUI().text, "Hello, World!")
     }
 }

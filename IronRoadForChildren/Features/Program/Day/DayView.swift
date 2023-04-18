fcdd8fe9d021d932edef8db8f3da6a545a270d99
Robert Zavaczki <robert.zavaczki@edu.fh-joanneum.at>
2023-04-18 17:34:03 +0200

    created a ScrollView with 10 program items

--- IronRoadForChildren/Features/Program/Day/DayView.swift
+++ IronRoadForChildren/Features/Program/Day/DayView.swift
@@ -13,15 +13,57 @@ struct DayView: View {
 	var body: some View {
 		VStack {
 			Divider()
-			Spacer()
-			Text(contents)
-			Spacer()
+			ScrollView {
+				ForEach(0 ..< 10) { _ in
+					ProgramItemView()
+						.padding(.top, 15)
+				}
+			}
 		}
+		.padding(.horizontal, 20)
 	}
 }
 
 struct DayView_Previews: PreviewProvider {
 	static var previews: some View {
 		DayView(contents: "Tagesprogramm")
+	}
+}
+
+struct ProgramItemView: View {
+	var body: some View {
+		Rectangle()
+			.cornerRadius(10)
+			.frame(height: 90)
+			.foregroundColor(.pink.opacity(0.03))
+			.overlay(
+				RoundedRectangle(cornerRadius: 20)
+					.strokeBorder(Color.gray, lineWidth: 1)
+			)
+			.overlay(
+				HStack {
+					Image(systemName: "person.fill")
+						.resizable()
+						.frame(height: 90)
+						.aspectRatio(1, contentMode: .fit)
+					VStack(alignment: .leading, spacing: 5) {
+						Text("THE BAND THAT HAS A VERY LONG NAME")
+							.fontWeight(.semibold)
+							.lineSpacing(5)
+						Text("STAGE HAS ALSO A LONG NAME")
+							.lineLimit(1)
+					}
+					.font(.system(size: 16))
+					// .background(Color.red)
+					Spacer()
+					Text("16:00 - 16:40")
+						.font(.system(size: 16))
+						.fontWeight(.semibold)
+						.padding(.trailing, 5)
+						.lineLimit(1)
+						.fixedSize(horizontal: true, vertical: false)
+				}
+				.padding(.horizontal, 10)
+			)
 	}
 }

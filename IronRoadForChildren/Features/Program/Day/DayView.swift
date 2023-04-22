603db1a89fb7717ac73151da73e7e69fbb6dfdbc
Alexander Kauer <contact@kaulex.dev>
2023-04-22 10:34:04 +0200

    fixed paddings and ScrollView

--- IronRoadForChildren/Features/Program/Day/DayView.swift
+++ IronRoadForChildren/Features/Program/Day/DayView.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 struct DayView: View {
 	var body: some View {
-		VStack {
-			Divider()
-			ScrollView {
+		ScrollView {
+			VStack {
+				Divider()
 				ForEach(0 ..< 10) { _ in
 					NavigationLink(destination: ProgramItemDelailView()) {
 						ProgramItemView()
-							.padding(.top, 15)
+							.padding(.top, 16)
+							.padding(.horizontal, 24)
 					}
 				}
 			}
+			.padding(.bottom, 16)
 		}
-		.padding(.horizontal, 20)
 	}
 }
 

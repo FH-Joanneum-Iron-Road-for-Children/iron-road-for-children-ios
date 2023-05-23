817b3f449d8e240098c3d7c73eb652d76a5f8424
Alexander Kauer <contact@kaulex.dev>
2023-05-23 10:43:34 +0200

    fixed line length violation

--- IronRoadForChildren/Features/More/MoreView.swift
+++ IronRoadForChildren/Features/More/MoreView.swift
@@ -9,7 +9,13 @@ import CoreUI
 import SwiftUI
 
 struct MoreView: View {
-	let text = "Die Iron Road for Children, kurz IRFC, ist Österreichs größtes markenoffenes Festival-Weekend für Bikes, Vespas und US-Cars. Der Eintritt für das komplette Event-Weekend ist für Besucher kostenlos, stattdessen werden unter dem Motto \"Ein Herz für Kinder - Benzin im Blut\", Spendengelder für erkrankte Kinder aus ganz Österreich gesammelt. Neben Ausfahrten mit den Fahrzeugen, zahlreichen Live Konzerten, einem Kinderbereich und einer Tattoo-Area wird den Besuchern auch eine große Austeller- und Streetfood-Area geboten. In diesem Jahr wird das Programm auch um eine Custom Bike Area erweitert und die IRFC ist Host der \"Internationalen Österreichischen Custom Bike Staatsmeisterschaft\". Die IRFC ist ein Event für die ganze Familie und zählt mittlerweile über 40.000 Besucher."
+	let text = """
+	Die Iron Road for Children, kurz IRFC, ist Österreichs größtes markenoffenes Festival-Weekend für Bikes, Vespas und US-Cars.
+	Der Eintritt für das komplette Event-Weekend ist für Besucher kostenlos, stattdessen werden unter dem Motto \"Ein Herz für Kinder - Benzin im Blut\", Spendengelder für erkrankte Kinder aus ganz Österreich gesammelt.
+	Neben Ausfahrten mit den Fahrzeugen, zahlreichen Live Konzerten, einem Kinderbereich und einer Tattoo-Area wird den Besuchern auch eine große Austeller- und Streetfood-Area geboten.
+	In diesem Jahr wird das Programm auch um eine Custom Bike Area erweitert und die IRFC ist Host der \"Internationalen Österreichischen Custom Bike Staatsmeisterschaft\".
+	Die IRFC ist ein Event für die ganze Familie und zählt mittlerweile über 40.000 Besucher.
+	"""
 
 	@Environment(\.openURL) var openURL
 

35afe58e888182487073477ae2733562099b677b
DanielZellner <daniel.zellner@edu.fh-joanneum.at>
2023-06-04 19:25:15 +0200

    added a better outlining for the choosen band

--- IronRoadForChildren/Features/Vote/VoteBandItem.swift
+++ IronRoadForChildren/Features/Vote/VoteBandItem.swift
@@ -11,50 +11,55 @@ struct VoteBandItem: View {
 	@State var bandName: String = "Bandname"
 	@State var bandDescription: String = "Band description test test"
 	@State var choosedBand: Bool = false
-    var body: some View {
-        VStack {
-            VStack {
-                Image("irfcMap")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                
+	var body: some View {
+		VStack {
+			VStack {
+				Image("irfcMap")
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+
+				HStack {
+					VStack(alignment: .leading) {
+						Text(bandDescription)
+							.font(.headline)
+							.foregroundColor(.secondary)
+						Text(bandName)
+							.font(.title)
+							.fontWeight(.black)
+							.foregroundColor(.primary)
+							.lineLimit(3)
+					}
+					.layoutPriority(100)
+
+					Spacer()
+				}.padding()
                 HStack {
-                    VStack(alignment: .leading) {
-                        Text(bandDescription)
-                            .font(.headline)
-                            .foregroundColor(.secondary)
-                        Text(bandName)
-                            .font(.title)
-                            .fontWeight(.black)
-                            .foregroundColor(.primary)
-                            .lineLimit(3)
-                    }
-                    .layoutPriority(100)
-                    
+                    Button(action: {
+                                        choosedBand = !choosedBand
+                                    }, label: {
+                                        Text(choosedBand ? "Abwählen" : "Auswählen").padding()
+
+                                    }).background(Color.irfcYellow)
+                                        .clipShape(Capsule())
                     Spacer()
                 }
-                .padding()
-                Button(action: {
-                    choosedBand = !choosedBand
-                }, label: {
-                    Text(choosedBand ? "Abwählen" : "Auswählen").padding()
-                    
-                }).background(!choosedBand ? Color.irfcYellow : Color.white)
-                    .clipShape(Capsule())
-                    .padding()
-            }
-            .cornerRadius(10)
-            .overlay(RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 1)
-            )
-            .background(Color.white)
-        }.cornerRadius(10)
-            .overlay(RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 1)
-            )
-            .padding(4)
-            .background(choosedBand ? Color.irfcYellow : Color.clear)
-    }
+				
+				
+					.padding()
+			}
+			.cornerRadius(10)
+			.overlay(RoundedRectangle(cornerRadius: 10)
+				.stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 5)
+			)
+			.background(Color.white)
+		}.cornerRadius(20)
+            .frame(width: 300)
+			.overlay(RoundedRectangle(cornerRadius: 10)
+				.stroke(choosedBand ? Color.irfcYellow : Color.clear, lineWidth: 5)
+			)
+			.padding(4)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+	}
 }
 
 struct VoteBandItem_Previews: PreviewProvider {

8273af5ee6179b37759e558bef3fba70cda31819
Alexander Kauer <contact@kaulex.dev>
2023-07-08 13:01:39 +0200

    Update gems & reformat

--- IronRoadForChildren/SupportingFiles/PreviewContent/ModelMocks.swift
+++ IronRoadForChildren/SupportingFiles/PreviewContent/ModelMocks.swift
@@ -13,10 +13,12 @@ struct Mocks {
 	}
 
 	static var voting: Voting {
-		return Voting(votingId: 1,
-		              title: "Bester Sänger",
-		              active: true,
-		              events: [event2, event3, event4])
+		return Voting(
+			votingId: 1,
+			title: "Bester Sänger",
+			active: true,
+			events: [event2, event3, event4]
+		)
 	}
 
 	static var events: [Event] {
@@ -28,78 +30,107 @@ struct Mocks {
 	}
 
 	static var event: Event {
-		Event(eventId: 1,
-		      title: "Tattoo Event 1",
-		      picture: picture,
-		      startDateTimeInUTC: Date(),
-		      endDateTimeInUTC: Date() + 1,
-		      eventLocation: eventLocation,
-		      eventCategory: eventCategory,
-		      eventInfo: eventInfo2)
+		Event(
+			eventId: 1,
+			title: "Tattoo Event 1",
+			picture: picture,
+			startDateTimeInUTC: Date(),
+			endDateTimeInUTC: Date() + 1,
+			eventLocation: eventLocation,
+			eventCategory: eventCategory,
+			eventInfo: eventInfo2
+		)
 	}
 
 	static var event2: Event {
-		Event(eventId: 2,
-		      title: "Musik Event 2",
-		      picture: picture2,
-		      startDateTimeInUTC: Date() + 3600,
-		      endDateTimeInUTC: Date() + 7200,
-		      eventLocation: eventLocation,
-		      eventCategory: eventCategory2,
-		      eventInfo: eventInfo)
+		Event(
+			eventId: 2,
+			title: "Musik Event 2",
+			picture: picture2,
+			startDateTimeInUTC: Date() + 3600,
+			endDateTimeInUTC: Date() + 7200,
+			eventLocation: eventLocation,
+			eventCategory: eventCategory2,
+			eventInfo: eventInfo
+		)
 	}
 
 	static var event3: Event {
-		Event(eventId: 3,
-		      title: "Musik Event",
-		      picture: picture,
-		      startDateTimeInUTC: Date() + 36000,
-		      endDateTimeInUTC: Date() + 370_000,
-		      eventLocation: eventLocation,
-		      eventCategory: eventCategory2,
-		      eventInfo: eventInfo)
+		Event(
+			eventId: 3,
+			title: "Musik Event",
+			picture: picture,
+			startDateTimeInUTC: Date() + 36000,
+			endDateTimeInUTC: Date() + 370_000,
+			eventLocation: eventLocation,
+			eventCategory: eventCategory2,
+			eventInfo: eventInfo
+		)
 	}
 
 	static var event4: Event {
-		Event(eventId: 4,
-		      title: "Musik Event",
-		      picture: picture2,
-		      startDateTimeInUTC: Date() + 110_000,
-		      endDateTimeInUTC: Date() + 120_000,
-		      eventLocation: eventLocation,
-		      eventCategory: eventCategory2,
-		      eventInfo: eventInfo)
+		Event(
+			eventId: 4,
+			title: "Musik Event",
+			picture: picture2,
+			startDateTimeInUTC: Date() + 110_000,
+			endDateTimeInUTC: Date() + 120_000,
+			eventLocation: eventLocation,
+			eventCategory: eventCategory2,
+			eventInfo: eventInfo
+		)
 	}
 
 	static var picture: Picture {
-		Picture(pictureId: 1,
-		        altText: "Test alt text wuhuu",
-		        path: "https://www.nasa.gov/sites/default/files/thumbnails/image/spacex_dragon_june_29.jpg")
+		Picture(
+			pictureId: 1,
+			altText: "Test alt text wuhuu",
+			path: "https://www.nasa.gov/sites/default/files/thumbnails/image/spacex_dragon_june_29.jpg"
+		)
 	}
 
 	static var picture2: Picture {
-		Picture(pictureId: 2,
-		        altText: "Test alt text wuhuu",
-		        path: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/afrc2021-0149-02_orig.jpg")
+		Picture(
+			pictureId: 2,
+			altText: "Test alt text wuhuu",
+			path: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/afrc2021-0149-02_orig.jpg"
+		)
 	}
 
 	static var eventLocation: EventLocation {
-		EventLocation(eventLocationId: 1, name: "Nova Rock Bühne")
+		EventLocation(
+			eventLocationId: 1,
+			name: "Nova Rock Bühne"
+		)
 	}
 
 	static var eventCategory: EventCategory {
-		EventCategory(eventCategoryId: 1, name: "Tattoo")
+		EventCategory(
+			eventCategoryId: 1,
+			name: "Tattoo"
+		)
 	}
 
 	static var eventCategory2: EventCategory {
-		EventCategory(eventCategoryId: 2, name: "Musik")
+		EventCategory(
+			eventCategoryId: 2,
+			name: "Musik"
+		)
 	}
 
 	static var eventInfo: EventInfo {
-		EventInfo(eventInfoId: 1, infoText: "My event info", pictures: [])
+		EventInfo(
+			eventInfoId: 1,
+			infoText: "My event info",
+			pictures: []
+		)
 	}
 
 	static var eventInfo2: EventInfo {
-		EventInfo(eventInfoId: 2, infoText: "My event info", pictures: [picture, picture])
+		EventInfo(
+			eventInfoId: 2,
+			infoText: "My event info",
+			pictures: [picture, picture]
+		)
 	}
 }

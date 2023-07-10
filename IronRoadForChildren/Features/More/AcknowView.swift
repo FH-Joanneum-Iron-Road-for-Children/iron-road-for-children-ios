5225b70812805ab74b664e35d6a4ad09b2e3a1b3
Alexander Kauer <contact@kaulex.dev>
2023-07-10 19:24:33 +0200

    Added AckGen as a acknowledgement list generator

---
+++ IronRoadForChildren/Features/More/AcknowView.swift
@@ -0,0 +1,21 @@
+//
+//  AcknowView.swift
+//  IronRoadForChildren
+//
+//  Created by Alexander Kauer on 10.07.23.
+//
+
+import AckGenUI
+import SwiftUI
+
+struct AcknowView: View {
+	var body: some View {
+		AcknowledgementsList()
+	}
+}
+
+struct AcknowView_Previews: PreviewProvider {
+	static var previews: some View {
+		AcknowView()
+	}
+}
